117f030326ee3a66cac11812761fb7d08f8a9848
Anthony Giretti <anthony.giretti@gmail.com>
2018-01-15 23:59:04 -0500

    Video streaming gallery connected to streaming web API

--- StreamingVideoGallery/src/app/app.module.ts
+++ StreamingVideoGallery/src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from './app.component';
-
+import { VgBufferingModule } from 'videogular2/buffering';
+import { VgControlsModule } from 'videogular2/controls';
+import { VgCoreModule } from 'videogular2/core';
+import { VgOverlayPlayModule } from 'videogular2/overlay-play';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
